8b4df7656af5a7b9aefbcca21fc99a584a68ec0a
Lajos Kovács <kovacs.lajos347@gmail.com>
2024-02-28 15:29:05 +0100

    add XC_GridL  class

--- calc3kv.py
+++ calc3kv.py
@@ -58,7 +58,15 @@ Builder.load_string('''
     TabbedPanelItem:
         text: 'XC'
         BoxLayout:           
+            orientation: 'vertical'   
+            size_hint: 1, 1/3
+            PageLabel:				
+                text: 'Kapacitív reaktancia'	
+                size_hint: 1, 1/8
 
+            XC_GridL:
+                size_hint: 1, 7/8
+ 
 
     TabbedPanelItem:
         text: 'fo'
@@ -103,7 +111,32 @@ Builder.load_string('''
 		font_size: '16sp'
         foreground_color:1,0,0,1
 
-
+<XC_GridL>:
+    cols: 2
+    padding: '10dp'
+    spacing: '20dp'
+        
+    Button:
+        text: 'Frekvencia, f (Hz)'
+        color: 0.5, 0.6, 0.7, 1
+    TextInput:				
+        text: 'ha'		
+		font_size: '16sp'
+        foreground_color:1,0,0,1
+    Button:
+        text: 'Kapacitás, C (nF)'
+        color: 0.5, 0.6, 0.7, 1
+    TextInput:				
+        text: 'ha'		
+		font_size: '16sp'
+        foreground_color:1,0,0,1
+    Button:
+        text: 'XC (ohm)'
+        color: 0.5, 0.6, 0.7, 1
+    TextInput:				
+        text: 'ha'		
+		font_size: '16sp'
+        foreground_color:1,0,0,1
 
 
 
@@ -118,6 +151,12 @@ class XL_GridL(GridLayout):
     pass
 
 
+class XC_GridL(GridLayout):
+    pass
+
+class Fo_GridL(GridLayout):
+    pass
+
 
 class EcalcWidget(TabbedPanel):
 
@@ -125,7 +164,6 @@ class EcalcWidget(TabbedPanel):
         super().__init__()
 
       
-
 
 
 class EcalcApp(App):
